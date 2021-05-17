08a4ba426863eed3dfc3babbe76db3c1d8bfa3f3
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-17 21:28:55 +1000

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function superbowlWin(objectArray) {
+  
+  
+  const resultOrUndefined = objectArray.find(({result}) => result === "W")
 
+  if (resultOrUndefined === undefined) {
+    return undefined
+     
+  }
+  else return resultOrUndefined.year
+  
+}
